8cbeeb73a229446c0c3df9d244233247a621285d
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2018-10-24 19:24:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
   </head>
 <body>  
 <?php  
-    echo "Hello, world! ciao a tutti";  
+    echo "Hello, world! ciao a tutti";
+    date_default_timezone_set('Europe/Rome');  
+  
+    echo "Ciao, oggi è il ";  
+    echo date("d-m-Y");  
+    echo " e sono le ore ";  
+    echo date("G:i");  
 ?>  
 </body>  
 </html>
