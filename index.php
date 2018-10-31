199768bdf315f74d764e0284ffe56c5298e09415
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2018-10-31 18:44:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,44 +41,4 @@
 </center>
 </body>
 </html>
-    
-<?php
-/*
-
-echo "<form action=risposta.php method=post>";
-echo "<center>";
-echo "<table width=50% bgcolor="909090">";
- echo "<tr>";
-  echo "<td>";
-   echo "<font face=verdana size=2>Nome</font>";
-   echo "<input type=text size=10 name=nome>";
-  echo "</td>";
-  echo "<td>";
-   echo "<font face=verdana size=2>Cognome</font>";
-   echo "<input type=text size=10 name=cognome>";
-  echo "</td>";
- echo "</tr>";
- echo "<tr>";
-  echo "<td>";
-   echo "<font face=verdana size=2>Login</font>";
-   echo "<input type=text size=10 name=login>";
-  echo "</td>";
-  echo "<td>";
-   echo "<font face=verdana size=2>password</font>";
-   echo "<input type=text size=10 name=password>";
-  echo "</td>";
- echo "</tr>";
- echo "<tr>";
-  echo "<td>";
-echo "<SELECT size=1 name=op> <OPTION selected></OPTION> <OPTION value=dati>Visualizza dati</OPTION> <OPTION value=check>Esegui login</OPTION></SELECT>";
-  echo "</td>";
-  echo "<td>";
-   echo "<input type=submit value=invia>";
-   echo "<input type=reset value=cancella>";
-  echo "</td>";
-echo " </tr>";
-echo "</table>";
-echo "</form>";
-echo "</center>";*/
-?>
 
