71c7e7a09244054a2728f65ca56072b687926873
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2018-11-05 22:53:35 +0100

    Update risposta.php

--- risposta.php
+++ risposta.php
@@ -10,7 +10,7 @@ if (($login=='mamma') and ($pass=='ciao'))
 	 echo"Login o Password SBAGLIATA";
  };
 }
-function dati($nome, $cognome, $data,$indirizzo)
+function dati($nome, $cognome, $data,$giorno,$mese,$anno,$indirizzo)
 {
 	
 	
@@ -21,6 +21,10 @@ function dati($nome, $cognome, $data,$indirizzo)
 			Il tuo nome e': $nome <br>
 			Il tuo cognome e': $cognome <br>
 			La tua data di nascita (come metodo date )e': $data <br>
+            
+            il tuo giorno di nascita e': $giorno<br>
+             il tuo mese di nascita e': $mese<br>
+              il tuo anno di nascita e': $anno<br>
 			Il tuo indirizzo e': $indirizzo <br>
 		</fieldset>";
 }
@@ -29,27 +33,23 @@ $op=$_POST['op'];
 $nome=$_POST['nome'];
 $cognome=$_POST['cognome'];
 $data = date('d-m-Y', strtotime($_POST['data']));
-
 $indirizzo=$_POST['indirizzo'];
 $login=$_POST['login'];
 $pass=$_POST['password'];
- 
-  
+
+$giorno=$_POST['giorno'];
+$mese=$_POST['mese'];
+$anno=$_POST['anno'];
 switch ($op){
-
       case "dati":
-		   dati($nome,$cognome,$data,$indirizzo);
+		   dati($nome,$cognome,$data,$indirizzo,$giorno,$mese,$anno);
            break;
-
       case "check":
            contrpass($login,$pass);
            break;
-
       default:
-           dati($nome,$cognome,$data,$indirizzo);
+           dati($nome,$cognome,$data,$indirizzo,$giorno,$mese,$anno);
            break;
-
-
+           }
  ?>
- 
  
