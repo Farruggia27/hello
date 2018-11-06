1a894fd8280b591a7f4286cfa31be4aa72872a40
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2018-11-06 12:16:36 +0100

    Update risposta.php

--- risposta.php
+++ risposta.php
@@ -10,7 +10,7 @@ if (($login=='mamma') and ($pass=='ciao'))
 	 echo"Login o Password SBAGLIATA";
  };
 }
-function dati($nome, $cognome, $data,$giorno,$mese,$anno,$indirizzo)
+function dati($nome, $cognome, $data,$giorno,$mese,$anno,$indirizzo,$email)
 {
 	
 	
@@ -26,7 +26,45 @@ function dati($nome, $cognome, $data,$giorno,$mese,$anno,$indirizzo)
              il tuo mese di nascita e': $mese<br>
               il tuo anno di nascita e': $anno<br>
 			Il tuo indirizzo e': $indirizzo <br>
+			La tua email e': $email<br>
 		</fieldset>";
+}
+ function chkEmail($email)
+{
+	// elimino spazi, "a capo" e altro alle estremità della stringa
+	$email = trim($email);
+
+	// se la stringa è vuota sicuramente non è una mail
+	if(!$email) {
+		return false;
+	}
+
+	// controllo che ci sia una sola @ nella stringa
+	$num_at = count(explode( '@', $email )) - 1;
+	if($num_at != 1) {
+		return false;
+	}
+
+	// controllo la presenza di ulteriori caratteri "pericolosi":
+	if(strpos($email,';') || strpos($email,',') || strpos($email,' ')) {
+		return false;
+	}
+
+	// la stringa rispetta il formato classico di una mail?
+	if(!preg_match( '/^[\w\.\-]+@\w+[\w\.\-]*?\.\w{1,4}$/', $email)) {
+		return false;
+	}
+
+	return true;
+}
+
+$email = 'test@dominio.it';
+
+if(chkEmail($email)) {
+	echo 'Indirizzo email corretto';
+}
+else {
+	echo 'Indirizzo email errato';
 }
 /******* Programma principale *******/
 $op=$_POST['op'];
@@ -36,20 +74,24 @@ $data = date('d-m-Y', strtotime($_POST['data']));
 $indirizzo=$_POST['indirizzo'];
 $login=$_POST['login'];
 $pass=$_POST['password'];
-
+$email=$_POST['email'];
 $giorno=$_POST['giorno'];
 $mese=$_POST['mese'];
 $anno=$_POST['anno'];
 switch ($op){
       case "dati":
-		   dati($nome,$cognome,$data,$giorno,$mese,$anno,$indirizzo);
+		chkEmail($email)   
+		dati($nome,$cognome,$data,$giorno,$mese,$anno,$indirizzo,$email);
            break;
       case "check":
            contrpass($login,$pass);
            break;
       default:
-           dati($nome,$cognome,$data,$giorno,$mese,$anno,$indirizzo);
+           	chkEmail($email)
+		dati($nome,$cognome,$data,$giorno,$mese,$anno,$indirizzo,$email);
            break;
            }
- ?>
+
+?>
+
  
